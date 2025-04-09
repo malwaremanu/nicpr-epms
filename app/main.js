f7e293d6cc0c82672bc34dadd75ad5854d6d8599
malwaremanu <tech.manujpr@gmail.com>
2025-04-09 06:23:02 +0530

    stackblitz testing

--- app/main.js
+++ app/main.js
@@ -15,7 +15,11 @@ createApp({
     Contact
   },
   template: `
-    <nav class="flex gap-4 mb-6">
+  <div> 
+    This is some change from stackblitz.
+  </div>
+  
+    <nav class="flex gap-4 mb-6">    
       <button @click="currentPage = 'Home'" class="bg-blue-500 text-white px-4 py-2 rounded">Home</button>
       <button @click="currentPage = 'About'" class="bg-green-500 text-white px-4 py-2 rounded">About</button>
       <button @click="currentPage = 'Contact'" class="bg-purple-500 text-white px-4 py-2 rounded">Contact</button>
